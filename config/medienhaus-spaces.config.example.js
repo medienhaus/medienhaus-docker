c9209200117e74d3e6c67206f51450857f78aca3
Andi Rueckel <andirueckel@users.noreply.github.com>
2023-02-15 12:35:59 +0100

    add .env.example && use env vars in config files && adapt .gitignore

---
+++ config/medienhaus-spaces.config.example.js
@@ -0,0 +1,46 @@
+const WebpackConfig = require('./webpack.config.js');
+
+module.exports = {
+    publicRuntimeConfig: {
+        name: '${SPACES_APP_PREFIX}/spaces',
+        authProviders: {
+            matrix: {
+                baseUrl: '${HTTP_SCHEMA}://matrix.${SPACES_HOSTNAME}',
+                api: '${HTTP_SCHEMA}://api.${SPACES_HOSTNAME}',
+                allowCustomHomeserver: true,
+            },
+            matrixContentStorage: {
+                baseUrl: '${HTTP_SCHEMA}://matrix.${SPACES_HOSTNAME}',
+            },
+            write: {
+                baseUrl: '${HTTP_SCHEMA}://write.${SPACES_HOSTNAME}/p',
+                api: '${HTTP_SCHEMA}://write.${SPACES_HOSTNAME}/mypads/api'
+            },
+            sketch: {
+                baseUrl: '${HTTP_SCHEMA}://sketch.${SPACES_HOSTNAME}',
+            },
+        },
+        contextRootSpaceRoomId: '${MATRIX_CONTEXT_ROOT}',
+        account: {
+            allowAddingNewEmails: true,
+        },
+        chat: {
+            pathToElement: '${HTTP_SCHEMA}://${SPACES_HOSTNAME}/element',
+        },
+        cms: {
+            path: '${HTTP_SCHEMA}://cms.${SPACES_HOSTNAME}',
+        },
+        activity: {
+            allowedTemplates: [
+                'event',
+                'resource',
+                'article'
+            ],
+        },
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    webpack: WebpackConfig,
+};
+
