20dcad13e089b6b2b497e6ceb2a885d63c440dad
Andi Rueckel <andirueckel@users.noreply.github.com>
2023-03-10 15:30:32 +0000

    strip example keyword from medienhaus-spaces.config.js file name

--- config/medienhaus-spaces.config.js
+++ config/medienhaus-spaces.config.js
@@ -6,12 +6,12 @@ module.exports = {
         authProviders: {
             matrix: {
                 baseUrl: '${HTTP_SCHEMA}://matrix.${SPACES_HOSTNAME}',
-                api: '${HTTP_SCHEMA}://api.${SPACES_HOSTNAME}',
+                // api: '${HTTP_SCHEMA}://api.${SPACES_HOSTNAME}',
                 allowCustomHomeserver: true,
             },
-            matrixContentStorage: {
-                baseUrl: '${HTTP_SCHEMA}://matrix.${SPACES_HOSTNAME}',
-            },
+            // matrixContentStorage: {
+            //     baseUrl: '${HTTP_SCHEMA}://matrix.${SPACES_HOSTNAME}',
+            // },
             write: {
                 baseUrl: '${HTTP_SCHEMA}://write.${SPACES_HOSTNAME}/p',
                 api: '${HTTP_SCHEMA}://write.${SPACES_HOSTNAME}/mypads/api'
@@ -20,23 +20,23 @@ module.exports = {
                 baseUrl: '${HTTP_SCHEMA}://sketch.${SPACES_HOSTNAME}',
             },
         },
-        contextRootSpaceRoomId: '${MATRIX_CONTEXT_ROOT}',
-        account: {
-            allowAddingNewEmails: true,
-        },
+        // contextRootSpaceRoomId: '${MATRIX_CONTEXT_ROOT}',
+        // account: {
+        //     allowAddingNewEmails: true,
+        // },
         chat: {
             pathToElement: '${HTTP_SCHEMA}://${SPACES_HOSTNAME}/element',
         },
-        cms: {
-            path: '${HTTP_SCHEMA}://cms.${SPACES_HOSTNAME}',
-        },
-        activity: {
-            allowedTemplates: [
-                'event',
-                'resource',
-                'article'
-            ],
-        },
+        // cms: {
+        //     path: '${HTTP_SCHEMA}://cms.${SPACES_HOSTNAME}',
+        // },
+        // activity: {
+        //     allowedTemplates: [
+        //         'event',
+        //         'resource',
+        //         'article'
+        //     ],
+        // },
     },
     eslint: {
         ignoreDuringBuilds: true,
