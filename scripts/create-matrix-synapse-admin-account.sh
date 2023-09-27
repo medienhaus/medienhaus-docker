002cb288e8201b81b438d1a01d1cf5d2663967f6
Andi Rueckel <andirueckel@users.noreply.github.com>
2023-09-27 16:08:42 +0200

    chore: update scripts/create-matrix-synapse-admin-account.sh

--- scripts/create-matrix-synapse-admin-account.sh
+++ scripts/create-matrix-synapse-admin-account.sh
@@ -18,7 +18,7 @@ fi
 
 set -o allexport && source .env && set +o allexport
 
-# -- create matrix-synapse root account ----------------------------------------
+# -- create matrix-synapse admin account ---------------------------------------
 
 docker exec matrix-synapse \
   register_new_matrix_user http://localhost:8008 \
